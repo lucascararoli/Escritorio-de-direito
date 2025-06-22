312f21247e4bde1cfa2bdb92d22e1aa4ea9684a6
Lucas Casaroli <143894301+lucascararoli@users.noreply.github.com>
2025-06-22 12:46:16 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAg7_Up9HWkq6P5tEB_ELEFEqd1EobveUk",
-  authDomain: "escritorio-direito-b6bb8.firebaseapp.com",
-  projectId: "escritorio-direito-b6bb8",
-  storageBucket: "escritorio-direito-b6bb8.firebasestorage.app",
-  messagingSenderId: "922868836048",
-  appId: "1:922868836048:web:01de4dbcc12ab8523960d5"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: "
 };
 
 const app = initializeApp(firebaseConfig);
